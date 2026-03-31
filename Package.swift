95823d10fa83631e434259ec656562445e7feffd
Vinoth N <vinoth.nv@zohocorp.com>
2026-03-31 12:07:29 +0530

    Moved dependency to single target

--- Package.swift
+++ Package.swift
@@ -11,10 +11,9 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "ZohoPortalAuth", targets: ["ZohoPortalAuth"]),
         .library(
             name: "ZohoDeskPortalAPIKit",
-            targets: ["ZohoDeskPortalAPIKit"]),
+            targets: ["ZohoDeskPortalAPIKit", "ZohoPortalAuth"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
