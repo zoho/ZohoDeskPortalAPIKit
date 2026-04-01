f258bc28c4bdeb5e9fa9661c46774ae50a3c7366
Vinoth N <vinoth.nv@zohocorp.com>
2026-04-01 12:24:44 +0530

    4.6.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "4.5.8"
+let version = "4.6.0"
 let package = Package(
     name: "ZohoDeskPortalAPIKit",
     platforms: [
@@ -19,6 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ZohoPortalAuth", url: "https://maven.zohodl.com/com/zoho/accounts/ios/zpauth/V1.1.0/Accounts_ios_clientportalFW_XCFramework.zip", checksum: "d4aebe71d11db2c878a00e65506542f20e111f8abb7a0dbc908630b4ee61e676"),
-        .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/\(version)/ZohoDeskPortalAPIKit.zip", checksum: "9bae4a184e8629e04c937df77871fc620914143dcd228afd6e9b0e8a504a1d2d")
+        .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/\(version)/ZohoDeskPortalAPIKit.zip", checksum: "e7bd0740c3fd7635ec6eccf45c13a61536a76ac0bb2a79cd5c81df6886dbfef6")
     ]
 )
