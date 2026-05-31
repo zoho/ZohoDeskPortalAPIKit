0f5dc4433d83093191d02ac831aaebdfd90e25b9
Vinoth N <vinoth.nv@zohocorp.com>
2026-05-31 14:42:47 +0530

    4.6.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "4.6.2"
+let version = "4.6.3"
 let package = Package(
     name: "ZohoDeskPortalAPIKit",
     platforms: [
@@ -20,6 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ZIAMCrypt", url: "https://maven.zohodl.com/ZIAMCrypt/iOS/0.1.5/XCFramework.zip", checksum: "005d0128fa1573689f777e415a38d4bf1ade202908f7d26f451281b25e66f065"),
         .binaryTarget(name: "ZohoPortalAuth", url: "https://maven.zohodl.com/com/zoho/accounts/ios/zpauth/V1.1.0/Accounts_ios_clientportalFW_XCFramework.zip", checksum: "d4aebe71d11db2c878a00e65506542f20e111f8abb7a0dbc908630b4ee61e676"),
-        .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/\(version)/ZohoDeskPortalAPIKit.zip", checksum: "3692265371169289771f9ad27cb4771eac7a8306bfc6d8043e2017cd1fa32909")
+        .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/\(version)/ZohoDeskPortalAPIKit.zip", checksum: "9fc25e1dd5d9c39a1666ae9600985d90aea078d28b5f1932e5c979be2918b3bb")
     ]
 )
